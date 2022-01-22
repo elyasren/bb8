ec10c465d898f70bdff7fb984cbdd54fcb9ef586
elyasren <98220866+elyasren@users.noreply.github.com>
2022-01-22 15:30:49 +0100

    wip

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 basic.forever(function () {
+    k_Bit.run(DIR.RunForward, 60)
     if (k_Bit.obstacle(MotorObs.LeftSide) == 0 && k_Bit.obstacle(MotorObs.RightSide) == 0) {
-    	
+        k_Bit.run(DIR.RunForward, 60)
     } else if (k_Bit.obstacle(MotorObs.LeftSide) == 0 && k_Bit.obstacle(MotorObs.RightSide) == 1) {
-    	
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 13)
     } else if (k_Bit.obstacle(MotorObs.LeftSide) == 1 && k_Bit.obstacle(MotorObs.RightSide) == 0) {
-    	
-    } else {
-    	
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 13)
     }
 })
